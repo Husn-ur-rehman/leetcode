d4def3f646b975efdf33ef7dbf6521e3d03d1eb6
HRK <khanhusnurrehman@gmail.com>
2026-08-01 19:52:52 +0500

    Problem 5 Around the Center Approach

--- LongestPalindromicSubstring.cpp
+++ LongestPalindromicSubstring.cpp
@@ -17,7 +17,7 @@ public:
         return s.substr(start,longestSPalindromeSubstring);
         
     }
-
+    //Helper Function (checks whether the substring is palindrome or not)
     bool isPalindrome(const string& s, int left, int right){
 
         while(left < right){
@@ -28,6 +28,30 @@ public:
         return true;
 
     }
+    //Helper Function return length;
+    int expand(const string& s, int left, int right){
+        while(left >= 0 && right < s.size() && s[left] == s[right]){
+            right++;
+            left--;
+        }
+        return right - left - 1;
+    }
+    //Around Center Approach O(n^2) Time complexity and O(1) space complexity
+    string AroundCenterApproach(string s){
+       int maxlength = 0, size= s.size(), start = 0;
+    
+       for(int i = 0 ; i < size ; i++ ){
+        int len1 = expand(s, i, i); // for odd ones;
+        int len2 = expand(s, i, i+1); // for even ones;
+            if(max(len1,len2) > maxlength){    
+                maxlength = max(len1,len2);
+                start = i - (maxlength-1)/2;
+            }
+
+       }
+       return s.substr(start,maxlength);
+
+    }
 
 };
 
@@ -36,7 +60,7 @@ int main(){
     Solution obj;
 
     for(int i = 0 ; i < arr.size(); i++){
-        cout<<obj.LongestPalindromicSubstring(arr[i])<<endl;
+        cout<<obj.AroundCenterApproach(arr[i])<<endl;
     }
     
 }
